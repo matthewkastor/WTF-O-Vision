a632b6e167af1744a6b90502c73586c82bbe2c81
Matthew Kastor <matthewkastor@gmail.com>
2013-01-12 00:33:55 -0500

    added setAsOptionalArg

--- WTF-O-Vision.js
+++ WTF-O-Vision.js
@@ -23,6 +23,35 @@ atropa = {};
 
 
 /**
+ * Set default values for optional function parameters.
+ * <pre>
+// To set a default value for an optional parameter
+function(optionalArg) {
+var defaultVal = 'hello there!';
+optionalArg = atropa.setAsOptionalArg(defaultVal, optionalArg);
+return optionalArg;
+}
+</pre>
+ * @author <a href="mailto:matthewkastor@gmail.com">
+ * Matthew Christopher Kastor-Inare III </a><br />
+ * ☭ Hial Atropa!! ☭
+ * @version 20120909
+ * @param {Mixed} defaultVal The default value to set.
+ * @param {Mixed} optionalArg A reference to the optional argument.
+ * @returns {Mixed} Returns the default value supplied when the optional
+ * argument is undefined or null. Otherwise, the supplied optional argument
+ * is returned.
+ */
+atropa.setAsOptionalArg = function (defaultVal, optionalArg) {
+	"use strict";
+	if (optionalArg === undefined || optionalArg === null) {
+		optionalArg = defaultVal;
+	}
+	return optionalArg;
+};
+
+
+/**
  * Container for all Glorious WTFifier related functions and such.
  * @author <a href="mailto:matthewkastor@gmail.com">
  * Matthew Christopher Kastor-Inare III </a><br />
