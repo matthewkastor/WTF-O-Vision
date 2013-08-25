0888a1fb86ce755b713d79645f616246f8e846cb
Matthew Kastor <matthewkastor@gmail.com>
2013-08-25 17:12:32 -0400

    rebuild

--- src/WTF-O-Vision.js
+++ src/WTF-O-Vision.js
@@ -667,7 +667,8 @@ atropa.inquire.isEmptyString = function (str) {
             [
                 atropa.regex,
                 atropa.string.countWords,
-                atropa.setAsOptionalArg
+                atropa.setAsOptionalArg,
+                atropa.wtf.dictionary
             ].forEach(function (prerequisite) {
                 if(prerequisite === undefined) {
                     supported = false;
@@ -708,8 +709,116 @@ atropa.inquire.isEmptyString = function (str) {
  * @namespace Container for all Glorious WTFifier related functions and such.
  * @see <a href="../../../AtropaToolboxTests.html?spec=atropa.wtf">tests</a>
  * @requires atropa.regex
+ * @requires atropa.wtf.dictionary
  */
 atropa.wtf = {};
+/**
+ * Accepts plain text input and Gloriously WTFifies it.
+ * @author <a href="mailto:matthewkastor@gmail.com">
+ *  Matthew Christopher Kastor-Inare III </a><br />
+ *  ☭ Hial Atropa!! ☭
+ * @version 20130110
+ * @param {String} target The text to WTFify.
+ * @param {Boolean} outputHTML Specifies if you want the output
+ *  in HTML format. If false, will output plain text. Defaults
+ *  to false.
+ * @return {String} Returns Genuine WTFified text.
+ */
+atropa.wtf.wtfify = function (target, outputHTML) {
+    "use strict";
+    atropa.supportCheck('wtf');
+    
+    var regexValue,
+        replacementText,
+        oldWord,
+        wtfCount,
+        wordCount,
+        ret,
+        word;
+    
+    if(true !== outputHTML) {
+        outputHTML = false;
+    }
+    ret = {};
+    wtfCount = 0;
+    target = target.trim();
+    wordCount = atropa.string.countWords(target);
+    if(true === outputHTML) {
+        target = target.replace(
+            /(\. ?){2,}/gi,
+            '<span style="color : brown ;"> [shit taco] </span>'
+        );
+        target = '<p> ' + target.replace(/(\r\n|\r|\n)/g,' <br/> ') + ' </p>';
+    } else {
+        target = target.replace(/(\. ?){2,}/gi, ' [shit taco] ');
+    }
+    /**
+     * Accepts plain text input and Gloriously WTFifies it.
+     * @author <a href="mailto:matthewkastor@gmail.com">
+     *  Matthew Christopher Kastor-Inare III </a><br />
+     *  ☭ Hial Atropa!! ☭
+     * @version 20130112
+     * @methodOf atropa.wtf.wtfify-
+     * @private
+     * @param {String} m First matched pattern in string searched.
+     * @param {String} sub1 First matched subpattern in string searched.
+     * @param {String} sub2 Second matched subpattern in string searched.
+     */
+    replacementText = function (m, sub1, sub2) {
+        wtfCount++;
+        sub1 = atropa.setAsOptionalArg('', sub1);
+        sub2 = atropa.setAsOptionalArg('', sub2);
+        var out;
+        if(true === outputHTML) {
+            out = '<span style="color : red ;">' +
+                sub1 + atropa.wtf.dictionary[word] + sub2 +
+                '</span>';
+        } else {
+            out = sub1 + atropa.wtf.dictionary[word] + sub2;
+        }
+        return out;
+    };
+    // word is defined in the containing scope and
+    // is not global, jshint is wrong
+    for (word in atropa.wtf.dictionary) {
+        if (atropa.wtf.dictionary.hasOwnProperty(word)) {
+            oldWord = atropa.regex.appendPrefixesAndSuffixes(word);
+            regexValue = new RegExp(oldWord, 'gi');
+            target = target.replace(regexValue, replacementText);
+        }
+    }
+    ret.wtfCount = wtfCount;
+    ret.wordCount = wordCount;
+    ret.score = wtfCount / wordCount;
+    ret.txt = target;
+    return ret;
+};
+/**
+ * WTFifies the <code>textContent</code> or <code>value</code> of the
+ *  given element and replaces the element's innerHTML with a pre block
+ *  containing the results of WTFification.
+ * @param {HTMLElement} elementReference A reference to an HTML Element.
+ * @returns {HTMLElement} Returns the given element after wtfification.
+ * @version 20130313
+ */
+atropa.wtf.htmlElement = function (elementReference) {
+    "use strict";
+    atropa.supportCheck('wtfHtmlElement');
+    
+    var wtfified, txt;
+    elementReference.innerHTML = elementReference.innerHTML.replace(
+        /<br>(\s+)?(\r\n|\r|\n)?/g, '\r\n');
+    txt = elementReference.value || elementReference.textContent;
+    wtfified = atropa.wtf.wtfify(txt, true);
+    elementReference.innerHTML =
+        '<pre style="color:black; background:white; white-space:pre-wrap;">' +
+        wtfified.txt +
+        '</pre>';
+    return elementReference;
+};
+
+
+
 /**
  * The Glorious WTFification Dictionary: Turning Shit
  * Into Polished Turds.
@@ -1177,112 +1286,6 @@ atropa.wtf.dictionary = {
     "i" : "Kevin",
     "you" : "Retards"
 };
-/**
- * Accepts plain text input and Gloriously WTFifies it.
- * @author <a href="mailto:matthewkastor@gmail.com">
- *  Matthew Christopher Kastor-Inare III </a><br />
- *  ☭ Hial Atropa!! ☭
- * @version 20130110
- * @param {String} target The text to WTFify.
- * @param {Boolean} outputHTML Specifies if you want the output
- *  in HTML format. If false, will output plain text. Defaults
- *  to false.
- * @return {String} Returns Genuine WTFified text.
- */
-atropa.wtf.wtfify = function (target, outputHTML) {
-    "use strict";
-    atropa.supportCheck('wtf');
-    
-    var regexValue,
-        replacementText,
-        oldWord,
-        wtfCount,
-        wordCount,
-        ret,
-        word;
-    
-    if(true !== outputHTML) {
-        outputHTML = false;
-    }
-    ret = {};
-    wtfCount = 0;
-    target = target.trim();
-    wordCount = atropa.string.countWords(target);
-    if(true === outputHTML) {
-        target = target.replace(
-            /(\. ?){2,}/gi,
-            '<span style="color : brown ;"> [shit taco] </span>'
-        );
-        target = '<p> ' + target.replace(/(\r\n|\r|\n)/g,' <br/> ') + ' </p>';
-    } else {
-        target = target.replace(/(\. ?){2,}/gi, ' [shit taco] ');
-    }
-    /**
-     * Accepts plain text input and Gloriously WTFifies it.
-     * @author <a href="mailto:matthewkastor@gmail.com">
-     *  Matthew Christopher Kastor-Inare III </a><br />
-     *  ☭ Hial Atropa!! ☭
-     * @version 20130112
-     * @methodOf atropa.wtf.wtfify-
-     * @private
-     * @param {String} m First matched pattern in string searched.
-     * @param {String} sub1 First matched subpattern in string searched.
-     * @param {String} sub2 Second matched subpattern in string searched.
-     */
-    replacementText = function (m, sub1, sub2) {
-        wtfCount++;
-        sub1 = atropa.setAsOptionalArg('', sub1);
-        sub2 = atropa.setAsOptionalArg('', sub2);
-        var out;
-        if(true === outputHTML) {
-            out = '<span style="color : red ;">' +
-                sub1 + atropa.wtf.dictionary[word] + sub2 +
-                '</span>';
-        } else {
-            out = sub1 + atropa.wtf.dictionary[word] + sub2;
-        }
-        return out;
-    };
-    // word is defined in the containing scope and
-    // is not global, jshint is wrong
-    for (word in atropa.wtf.dictionary) {
-        if (atropa.wtf.dictionary.hasOwnProperty(word)) {
-            oldWord = atropa.regex.appendPrefixesAndSuffixes(word);
-            regexValue = new RegExp(oldWord, 'gi');
-            target = target.replace(regexValue, replacementText);
-        }
-    }
-    ret.wtfCount = wtfCount;
-    ret.wordCount = wordCount;
-    ret.score = wtfCount / wordCount;
-    ret.txt = target;
-    return ret;
-};
-/**
- * WTFifies the <code>textContent</code> or <code>value</code> of the
- *  given element and replaces the element's innerHTML with a pre block
- *  containing the results of WTFification.
- * @param {HTMLElement} elementReference A reference to an HTML Element.
- * @returns {HTMLElement} Returns the given element after wtfification.
- * @version 20130313
- */
-atropa.wtf.htmlElement = function (elementReference) {
-    "use strict";
-    atropa.supportCheck('wtfHtmlElement');
-    
-    var wtfified, txt;
-    elementReference.innerHTML = elementReference.innerHTML.replace(
-        /<br>(\s+)?(\r\n|\r|\n)?/g, '\r\n');
-    txt = elementReference.value || elementReference.textContent;
-    wtfified = atropa.wtf.wtfify(txt, true);
-    elementReference.innerHTML =
-        '<pre style="color:black; background:white; white-space:pre-wrap;">' +
-        wtfified.txt +
-        '</pre>';
-    return elementReference;
-};
-
-
 
 /**
  * A few utilities for manipulating strings.
